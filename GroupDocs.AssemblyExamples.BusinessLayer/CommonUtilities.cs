20c6438c7f22cf7edb725e8436832ca84c3c1cd8
Muhammad Atir <atirtahir@hotmail.com>
2015-11-19 16:28:32 +0500

    GroupDocs.Assembly Examples

---
+++ GroupDocs.AssemblyExamples.BusinessLayer/CommonUtilities.cs
@@ -0,0 +1,56 @@
+﻿using GroupDocs.Assembly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupDocs.AssemblyExamples.BusinessLayer
+{
+    public class CommonUtilities
+    {
+
+        public const string sourceFolderPath = "../../../../Data/SourceFolder/";
+        public const string destinationFolderPath = "../../../../Data/DestinationFolder/";
+        public const string licensePath = "../../GroupDocs.Assembly Product Family.lic";
+
+        #region DocumentDirectories
+        public static string DocumentSourceFolderPath(string fileName, string sourceFolder)
+        {
+            return Path.Combine(Path.GetFullPath(sourceFolderPath), sourceFolder, fileName);
+        }
+
+        public static string DocumentOutputFolderPath(string fileName, string destinationFolder)
+        {
+            return Path.Combine(Path.GetFullPath(destinationFolderPath), destinationFolder, fileName);
+        }
+
+        #endregion
+
+        #region ProductLicense
+
+        public static void ProductLicense()
+        {
+            License lic = new License();
+            lic.SetLicense(licensePath);
+        }
+
+        #endregion
+
+        #region ChangeFileName
+
+        public static string ChangeFileName(string fileName)
+        {
+            string currentDateTime = DateTime.Now.ToString("dd-MM-yyyy-hh-mm-ss");
+            string nameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            string extension = Path.GetExtension(fileName);
+            string alterFileName = nameWithoutExtension + "-" + currentDateTime + "-CS";
+            string updatedFileName = alterFileName + extension;
+
+            return updatedFileName;
+        }
+
+        #endregion
+    }
+}
---
+++ GroupDocs.AssemblyExamples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroupDocs.AssemblyExamples.BusinessLayer;
+using GroupDocs.Assembly;
+
+namespace GroupDocs.AssemblyExamples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonUtilities.ProductLicense();
+
+            string folderName = "PowerPointTemplates";
+            string fileName = "Bubble Chart.pptx";
+            string dataSourceName = "orders";
+            string updatedFileName = CommonUtilities.ChangeFileName(fileName);
+
+            try
+            {
+                DocumentAssembler assembler = new DocumentAssembler();
+                assembler.AssembleDocument(CommonUtilities.DocumentSourceFolderPath(fileName, folderName), CommonUtilities.DocumentOutputFolderPath(updatedFileName, folderName), DataLayer.OrdersData(), dataSourceName);
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+            }
+           
+            //If dataSourceName is products call ProductsData data source 
+            //If dataSourceName is orders call OrdersData data source 
+            //If dataSourceName is customers call PopulateData data source
+            //If dataSourceName is customer call CustomerData data source
+        }
+    }
+}
+
